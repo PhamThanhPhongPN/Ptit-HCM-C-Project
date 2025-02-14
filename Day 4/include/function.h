2cbeaf6dcfdb4a8196c1ec95fb10852fc6f33bfe
Phạm Thanh Phong <92080858+PhamThanhPhongPN@users.noreply.github.com>
2025-02-14 08:24:47 +0700

    Add files via upload

---
+++ Day 4/include/function.h
@@ -0,0 +1,25 @@
+// tao cac function
+
+// ham hien menu chinh
+void ShowMain();
+
+// ham hien menu chinh sua danh muc
+void ShowCategory();
+
+// ham hien thi danh sach danh muc
+void ShowCategoryList(int a,Category CategoryList[]);
+
+// ham them vao mot danh muc moi
+void AddCategory(int a,Category CategoryList[]);
+
+// ham sua doi mot danh muc
+void ChangeCategory(int a,Category CategoryList[]); 
+
+// ham xoa mot danh muc theo Id
+void DeleteCategory(int a,Category CategoryList[]);
+
+// ham sap xep danh thuc 
+void SortCate(int a,Category CategoryList[]);
+
+// ham tim kiem danh muc theo ten
+void FindCategory(int a,Category CategoryList[]);
